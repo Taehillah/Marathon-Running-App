e448284b5812fcfb6452c66d808d86e0578715bb
fedora <ishmaelmafole@gmail.com>
2022-07-10 04:33:13 +0200

    Android Marathon Rating App uploaded.

--- app/src/main/java/com/example/marathon_rating_app/MainActivity.java
+++ app/src/main/java/com/example/marathon_rating_app/MainActivity.java
@@ -1,38 +1,93 @@
 package com.example.marathon_rating_app;
+//43385508
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
-import android.view.Window;
 import android.widget.Button;
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
-
+    /*
+    private EditText val1, val2;
+    private Button button;
+    private String hr;
+    private int min;*/
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+/*        val1 = findViewById(R.id.hours);
+        val2 = findViewById(R.id.minutes);
+        button = findViewById(R.id.button);
+*/
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle("First Activity");
 
-        Button button = (Button) findViewById(R.id.button);
+        //EditText
+        EditText diHours = findViewById(R.id.hours);
+        EditText diMinutes = findViewById(R.id.minutes);
+        //Button
+        Button buttone = findViewById(R.id.button);
 
-            button.setOnClickListener(new View.OnClickListener() {
+        //Button click listener
+        buttone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //data from Edit texts
+                String simplestDiHours = diHours.getText().toString();
+                String simplestDiMinutes = diMinutes.getText().toString();
+
+                //Intents to move data over
+                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                intent.putExtra("SIMPLESTDIHOURS",simplestDiHours);
+                intent.putExtra("SIMPLESTDIMINUTES",simplestDiMinutes);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
+        
+
+ /*           button.setOnClickListener(new View.OnClickListener() {
         @Override
-        public void onClick(View view) {
-            openActivity_main2();
+        public void onClick(View view)
+        {
+            sendData();
         }
     });
     }
 
-    public void openActivity_main2(){
-        Intent intent = new Intent(this, MainActivity2.class);
-        startActivity(intent);
-    }
+public void sendData() {
+    hr = val1.getText().toString().trim();
+    min = Integer.parseInt(val2.getText().toString().trim());
 
+    Intent i = new Intent(MainActivity.this, MainActivity2.class);
+
+    i.putExtra(MainActivity2.Hora.hr);
+    i.putExtra(MainActivity2.Motsotso.min);
+
+    startActivity(i);
 }
+}
+
+
+
+ //   private void startMainActivity2(){
+ //       Intent i = new Intent(this, MainActivity2.class);
+ //       i.putExtra("val1", 1);
+ //       i.putExtra("val2", 1);
+ //       startActivity(i);
+ //   }
+
+ //   public void openActivity_main2(){
+ //       Intent intent = new Intent(this, MainActivity2.class);
+//        startActivity(intent);
+//    }
+
+*/
--- app/src/main/java/com/example/marathon_rating_app/MainActivity2.java
+++ app/src/main/java/com/example/marathon_rating_app/MainActivity2.java
@@ -1,14 +1,69 @@
 package com.example.marathon_rating_app;
-
+//43385508
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity2 extends AppCompatActivity {
+
+public static final String Hora = "Hora";
+public static final String Motsotso = "Motsotso";
+
+    private EditText val1;
+    private EditText val2;
+    private Button button;
+    private String hr;
+    private int min;
+    private int defaultValue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle("Second Activity");
+
+        Intent intent = getIntent();
+        String simplestDiHours = intent.getStringExtra("SIMPLESTDIHOURS");
+        String simplestDiMinutes = intent.getStringExtra("SIMPLESTDIMINUTES");
+
+//textview fill
+        TextView simplestResult1 = findViewById(R.id.hours);
+        TextView simplestResult2 = findViewById(R.id.minutes);
+
+        simplestResult1.setText("Name "+simplestDiHours);
+
     }
 }
+
+
+/*
+        val1 = findViewById(R.id.hours);
+        val2 = findViewById(R.id.minutes);
+
+        Intent i = getIntent();
+        hr = i.getStringExtra(Hora);
+        min = i.getIntExtra(Motsotso, defaultValue);
+
+        val1.setText("Hi "+hr);
+        val2.setText("Hey "+min);
+
+       ImageButton backArrow = (ImageButton) findViewById(R.id.backArrow);
+        backArrow.setOnClickListener(new View.OnClickListener()){
+            @Override
+            public void onClick(View view) {
+                openMainActivity();
+            }
+        };
+}
+*/
