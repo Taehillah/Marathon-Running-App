e81e89cc0847b563f04c593c47fa6eb9f23d4ea5
fedora <ishmaelmafole@gmail.com>
2022-07-16 21:56:35 +0200

    Updated on crash

--- app/src/main/java/com/example/marathon_rating_app/MainActivity2.java
+++ app/src/main/java/com/example/marathon_rating_app/MainActivity2.java
@@ -5,13 +5,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.text.MessageFormat;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -24,6 +28,8 @@ public static final String Motsotso = "Motsotso";
     private String hr;
     private int min;
     private int defaultValue;
+    private Object Visible;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,8 +49,57 @@ public static final String Motsotso = "Motsotso";
 
         int huure = Integer.parseInt(simplestDiHours);
         int minute = Integer.parseInt(simplestDiMinutes);
+        int timeHours = huure * 60;
+        float timeRan = timeHours + minute;
+        float averagePerHour = (float) (timeRan/42.2);
 
-        simplestResult1.setText("Name "+huure*minute);
+        ImageView pictureGold = (ImageView) findViewById(R.id.imgGold);
+        ImageView pictureSilver = (ImageView)findViewById(R.id.imgSilver);
+        ImageView pictureBronze = (ImageView)findViewById(R.id.imgBronze);
 
+
+
+        if (averagePerHour<11)
+        {
+            pictureGold.setVisibility(View.VISIBLE);
+            simplestResult1.setText(MessageFormat.format("You have run: {0} minute(s) every Kilometer", averagePerHour));
+            simplestResult2.setText("Woooow! You have earned a Gold Medals");
+        }
+        else if (averagePerHour<15)
+        {
+
+            pictureSilver.setVisibility(View.VISIBLE);
+            simplestResult1.setText(MessageFormat.format("You have run: {0}minute(s) every Kilometer", averagePerHour));
+            simplestResult2.setText("Yeeey! You have earned a Silver Medals");
+        }
+        else
+        {
+            pictureBronze.setVisibility(View.VISIBLE);
+            simplestResult1.setText(MessageFormat.format("You have run: {0}minute(s) every Kilometer", averagePerHour));
+            simplestResult2.setText("You have earned a Bronze Medals");
+        }
+
+
+        //back Button
+        ImageButton backButton = findViewById(R.id.backArrow);
+
+        backButton.setOnClickListener(new View.OnClickListener()
+        {
+                                          @Override
+                                          public void onClick(View view) {
+                                              System.exit(0);
+                                          }
+        });
+
+            //exitButton
+            TextView exitButton = findViewById(R.id.exit);
+
+        exitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                finishAffinity();
+            }
+        });
+        }
     }
-}
